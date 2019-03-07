83721962776111f3efd8c644a32645a1686f1052
Wesley Rocha <wesley.rocha@hexagonagriculture.com>
2019-03-07 14:01:44 -0300

    Initial commit

---
+++ Assets/BoardController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour
+{
+    public GameObject tilePrefab;
+    public float minX = -4.5f;
+    public float maxX = 4.5f;
+    public float minY = -4.5f;
+    public float maxY = 4.5f;
+    
+    void Start()
+    {
+        for (float x = minX, i = 0; x <= maxX; x += 1f, i++) 
+        {
+            for (float y = maxY, j = 0; y >= minY; y -= 1f, j++) 
+            {
+                GameObject tile = Instantiate(tilePrefab, new Vector2(x, y), Quaternion.identity);
+                TileController tileController = tile.GetComponent<TileController>();
+                tileController.I = i;
+                tileController.J = j;
+            }
+        }
+    }
+}
---
+++ Assets/TileController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour
+{
+    public float I { get; set; }
+    public float J { get; set; }
+
+    void OnMouseDown()
+    {
+        Debug.Log("clicado: " + I + ", " + J);
+    }
+}
