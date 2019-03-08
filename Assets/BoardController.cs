2d1a9af8e8279149e155370f575e67ef49f4a990
Wesley Rocha <wesley.rocha@hexagonagriculture.com>
2019-03-08 14:06:34 -0300

    Initializing fields and setting bombs

--- Assets/BoardController.cs
+++ Assets/BoardController.cs
@@ -5,22 +5,69 @@ using UnityEngine;
 public class BoardController : MonoBehaviour
 {
     public GameObject tilePrefab;
-    public float minX = -4.5f;
-    public float maxX = 4.5f;
-    public float minY = -4.5f;
-    public float maxY = 4.5f;
+    public int fieldSize = 10;
+    public int amountBombs = 10;
+
+    private float maxY = 4.5f;
+    private float incrementY = -1f;
+    private float minX = -4.5f;
+    private float incrementX = 1f;
+    // private float maxX = 4.5f;
+    // private float minY = -4.5f;
+
+    private int[,] mineField;
+    private GameObject[,] screenField;
     
     void Start()
     {
-        for (float x = minX, i = 0; x <= maxX; x += 1f, i++) 
+        InitializeScreenField();
+        InitializeMineField();
+        SetBombs();
+    }
+
+    void InitializeScreenField()
+    {
+        screenField = new GameObject[fieldSize, fieldSize];
+
+        float y = maxY;
+        for (int j = 0; j < fieldSize; j++, y += incrementY)
         {
-            for (float y = maxY, j = 0; y >= minY; y -= 1f, j++) 
+            float x = minX;
+            for (int i = 0; i < fieldSize; i++, x += incrementX) 
             {
                 GameObject tile = Instantiate(tilePrefab, new Vector2(x, y), Quaternion.identity);
                 TileController tileController = tile.GetComponent<TileController>();
-                tileController.I = i;
-                tileController.J = j;
+                tileController.SetPosition(i, j);
+                screenField[i, j] = tile;
             }
+        }
+    }
+
+    void InitializeMineField()
+    {
+        mineField = new int[fieldSize, fieldSize];
+
+        for (int i = 0; i < fieldSize; i++)
+        {
+            for (int j = 0; j < fieldSize; j++)
+            {
+                mineField[i, j] = 0;
+            }
+        }
+    }
+
+    void SetBombs()
+    {
+        for (int bombs = 0; bombs < amountBombs; bombs++)
+        {
+            int i = Random.Range(0, fieldSize);
+            int j = Random.Range(0, fieldSize);
+            if (mineField[i,j] != 1)
+            {
+                mineField[i,j] = 1;
+                // método usado pra teste, provavelmente as bombas só existirão em mineField
+                screenField[i,j].GetComponent<TileController>().SetBomb();
+            } 
         }
     }
 }
--- Assets/TileController.cs
+++ Assets/TileController.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class TileController : MonoBehaviour
 {
-    public float I { get; set; }
-    public float J { get; set; }
+    public int i { get; private set; }
+    public int j { get; private set; }
+    // TODO verificar necessidade dessa variável, aparentemente não tem
+    public bool hasBomb = false;
+
+    public void SetPosition(int i, int j)
+    {
+        this.i = i;
+        this.j = j;
+    }
+
+    public void SetBomb()
+    {
+        this.hasBomb = true;
+    }
 
     void OnMouseDown()
     {
-        Debug.Log("clicado: " + I + ", " + J);
+        Debug.Log("clicado: " + i + ", " + j);
     }
 }
