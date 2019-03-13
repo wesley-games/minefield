f177ff0d20683e5ece10d8a4fb1a03c6b87b555c
Wesley Rocha <wesley.rocha@hexagonagriculture.com>
2019-03-13 14:03:53 -0300

    Adding Timer

--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class GameOverController : MonoBehaviour
+public class UIController : MonoBehaviour
 {
+    public GameObject timerLabel;
     public GameObject panel;
     public GameObject winLabel;
     public GameObject loseLabel;
     public GameObject buttonRestart;
 
+    private float timer = 0;
+    private float timerCount;
     private BoardController boardController;
 
     void Awake()
@@ -27,6 +31,17 @@ public class GameOverController : MonoBehaviour
     {
         BoardController.OnPlayerWin -= OnPlayerWin;
         BoardController.OnPlayerDead -= OnPlayerDead;
+    }
+
+    void Update()
+    {
+        if (timerCount > 1) {
+            timer++;
+            timerCount -= 1;
+        } else {
+            timerCount += Time.deltaTime;
+        }
+        timerLabel.GetComponent<Text>().text = this.convertNumberToTimeString(timer);
     }
 
     void OnPlayerWin()
@@ -53,5 +68,14 @@ public class GameOverController : MonoBehaviour
         loseLabel.SetActive(false);
         buttonRestart.SetActive(false);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private string convertNumberToTimeString(float number) {
+        int minutes = (int) number / 60;
+        int seconds = (int) number % 60;
+
+        string minutesString = minutes < 10 ? "0" + minutes : minutes.ToString();
+        string secondsString = seconds < 10 ? "0" + seconds : seconds.ToString();
+        return minutesString + ":" + secondsString;
     }
 }
